898937d1584fe65278a976c316cf953547defbef
rehmo <Janett.Herdt@stud.h-da.de>
2018-07-10 13:10:26 +0200

    beautified code

--- prototype_umsetzung/SpeechToText-WebSockets-Javascript-preview/samples/browser/sample-server.js
+++ prototype_umsetzung/SpeechToText-WebSockets-Javascript-preview/samples/browser/sample-server.js
@@ -8,11 +8,11 @@ var qrcode = require('qrcode-terminal'); // "qrcode-terminal": "^0.11.0",
 var sample = fs.readFileSync(__dirname + '/Sample.html', 'utf8');
 
 var enableTunnel = false;
-for (let j = 0; j < process.argv.length; j++) {  
+for (let j = 0; j < process.argv.length; j++) {
     enableTunnel |= process.argv[j] == 'enableTunnel';
 }
 
-var keyFile = __dirname+'/speech.key';
+var keyFile = __dirname + '/speech.key';
 if (fs.existsSync(keyFile)) {
     var key = fs.readFileSync(keyFile, 'utf8');
     key = key.replace(/\s/g, "");
@@ -26,35 +26,37 @@ if (fs.existsSync(keyFile)) {
 
 
 var port = 8765;
-var server = http.createServer(function(request, response){
-    var respond = function(status, data, contentType) {
-        if (typeof(contentType) === 'undefined') {
+var server = http.createServer(function (request, response) {
+    var respond = function (status, data, contentType) {
+        if (typeof (contentType) === 'undefined') {
             contentType = 'text/plain';
         }
-        response.writeHead(status, {'Content-Type': contentType});
+        response.writeHead(status, {
+            'Content-Type': contentType
+        });
         !!data && response.write(data);
         response.end();
     }
 
-    path= url.parse(request.url).pathname;    
+    path = url.parse(request.url).pathname;
     console.log("Incoming request:" + request.url);
 
     if (path == '/token') {
-        getToken(key, function(token){ 
+        getToken(key, function (token) {
             respond(200, token);
-         })
+        })
     } else if (path == '/') {
         respond(200, sample, 'text/html');
     } else {
-        var pathExists = fs.existsSync(__dirname + '/../../'+path);
+        var pathExists = fs.existsSync(__dirname + '/../../' + path);
 
-        if (!pathExists || 
+        if (!pathExists ||
             !path.endsWith('speech.sdk.bundle.js') &&
             !path.endsWith('speech.sdk.bundle.js.map')) {
             respond(404);
         } else {
             var type = 'application/javascript';
-            respond(200, fs.readFileSync(__dirname+'/../../'+path, 'utf8'), type);
+            respond(200, fs.readFileSync(__dirname + '/../../' + path, 'utf8'), type);
         }
     }
 });
@@ -63,22 +65,22 @@ var server = http.createServer(function(request, response){
 var quit = false;
 if (enableTunnel) {
     var localtunnel = require('localtunnel');
-    var tunnel = localtunnel(port, function(err, tunnel) {
+    var tunnel = localtunnel(port, function (err, tunnel) {
         if (err) {
             quit = true;
-            server.close(); 
+            server.close();
             console.log('Something went south...' + err.message)
         } else {
             printServerInfo(tunnel.url)
         }
     });
 
-    tunnel.on('close', function() {
+    tunnel.on('close', function () {
         quit = true;
-        server.close();   
+        server.close();
     });
 } else {
-    printServerInfo('http://'+os.hostname() + ':' + port);
+    printServerInfo('http://' + os.hostname() + ':' + port);
 }
 
 
@@ -90,7 +92,9 @@ if (!quit) {
 function printServerInfo(url) {
     console.log('Up and running @ ' + url);
     qrcode.setErrorLevel('H');
-    qrcode.generate(url, {small: true});
+    qrcode.generate(url, {
+        small: true
+    });
 }
 
 function getToken(apiKey, result) {
@@ -98,23 +102,23 @@ function getToken(apiKey, result) {
         host: 'api.cognitive.microsoft.com',
         path: '/sts/v1.0/issueToken',
         method: 'POST',
-         headers: {
+        headers: {
             'Content-type': 'application/x-www-form-urlencoded',
             'Content-Length': '0',
             'Ocp-Apim-Subscription-Key': apiKey
         }
     };
 
-    var callback = function(response) {
-      var token = ''
-      response.on('data', function (chunk) {
-        token += chunk;
-      });
+    var callback = function (response) {
+        var token = ''
+        response.on('data', function (chunk) {
+            token += chunk;
+        });
 
-      response.on('end', function () {
-        result(token);
-        console.log(token);
-      });
+        response.on('end', function () {
+            result(token);
+            console.log(token);
+        });
     }
 
     var issueTokenRequest = https.request(options, callback);
--- prototype_umsetzung/readme.md
+++ prototype_umsetzung/readme.md
@@ -1,8 +1,15 @@
 ## Instruction to get meyvn running in the browser (console needed)
 
 1. Navigate to "\meyvn\prototype_umsetzung\SpeechToText-WebSockets-Javascript-preview\samples\browser" in your console
+
 2. Open console and enter: **echo 80478e57fa7d42c3a7eefcb24b2c09ab > speech.key**  
-    Make sure the API Key is valid! Otherwise request a new one here: https://azure.microsoft.com/en-us/try/cognitive-services/
+    Make sure the API Key is valid! Otherwise request a new one (for free!) here: https://azure.microsoft.com/en-us/try/cognitive-services/
+
 3. Followed by: **node sample-server.js enableTunnel**
-4. Open **'Sample.html'** in the 'samples\browser' folder
+
+4. Open the generated URL  
+    This is what it could look like:  
+    λ node sample-server.js enableTunnel  
+    **Up and running @ https://wonderful-husky-76.localtunnel.me**
+
 5. Upon Start - Choose for Recognition Mode: **conversation**
